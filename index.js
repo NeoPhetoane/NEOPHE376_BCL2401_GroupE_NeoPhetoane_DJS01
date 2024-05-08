aa6b13e9c0180f2471e34c231475374bb675ab52
NeoPhetoane <phetoaneneo4@gmail.com>
2024-05-08 13:28:11 +0200

    Variable names changed to more specific ones.

--- index.js
+++ index.js
@@ -6,26 +6,26 @@
  */
 
 // Given Parameters
-const vel = 10000; // velocity (km/h)
-const acc = 3; // acceleration (m/s^2)
+const initialVelocity = 10000; // velocity (km/h)
+const acceleration = 3; // acceleration (m/s^2)
 const time = 3600; // seconds (1 hour)
-const d = 0; // distance (km)
-const fuel = 5000; // remaining fuel (kg)
-const fbr = 0.5; // fuel burn rate (kg/s)
+const initialDistance = 0; // distance (km)
+const startingFuel = 5000; // remaining fuel (kg)
+const fuelBurningRate = 0.5; // fuel burn rate (kg/s)
 
 
-const d2 = d + (vel*time) //calcultes new distance
-const rf = fbr*time //calculates remaining fuel
-const vel2 = calcNewVel(acc, vel, time) //calculates new velocity based on acceleration
+const endingDistance = initialDistance + (initialVelocity * time) //calcultes new distance
+const remainingFuel = startingFuel - fuelBurningRate * time //calculates remaining fuel
+const updatedVelocity = calculateNewVelocity(acceleration, initialVelocity, time) //calculates new velocity based on acceleration
 
 // Pick up an error with how the function below is called and make it robust to such errors
-calcNewVel = (vel, acc, time) => { 
-  return vel + (acc*time)
+calculateNewVelocity = (velocity, acceleration, time) => { 
+  return initialVelocity + (acceleration*time)
 }
 
-console.log(`Corrected New Velocity: ${vel2} km/h`);
-console.log(`Corrected New Distance: ${d2} km`);
-console.log(`Corrected Remaining Fuel: ${rf} kg`);
+console.log(`Corrected New Velocity: ${updatedVelocity} km/h`);
+console.log(`Corrected New Distance: ${endingDistance} km`);
+console.log(`Corrected Remaining Fuel: ${remainingFuel} kg`);
 
 
 
