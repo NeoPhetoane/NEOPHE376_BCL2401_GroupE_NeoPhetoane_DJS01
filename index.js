59f1461bbb22c34a7af6aa967d9d5f5f81d3336b
NeoPhetoane <phetoaneneo4@gmail.com>
2024-05-08 15:53:01 +0200

    Converted acceleration measurement systenm to per hour and moved the function to be declared before use.

--- index.js
+++ index.js
@@ -13,6 +13,15 @@ const initialDistance = 0; // distance (km)
 const startingFuel = 5000; // remaining fuel (kg)
 const fuelBurningRate = 0.5; // fuel burn rate (kg/s)
 
+
+//This function is moved up here to declare it before using it in updatedVelocity. This is to catch a possible error before attempting to use it so that the program does not crash.
+
+function calculateNewVelocity(initialVelocity, acceleration, time) { //function declared
+  const accelerationInKmh = acceleration * 3.6; //This decimal converts m/s^2 into km/h to match the measurement unit for velocity.
+  return initialVelocity + accelerationInKmh * time;
+};
+
+
 const endingDistance = initialDistance + initialVelocity * (time / 3600); //calcultes new distance. Divides the time by 3600 to convert it to hours to match the km/h of the velocity
 const remainingFuel = startingFuel - fuelBurningRate * time; //calculates remaining fuel by subtracting the burnt fuel and its time from what was initially there.
 const updatedVelocity = calculateNewVelocity(
@@ -22,9 +31,6 @@ const updatedVelocity = calculateNewVelocity(
 ); //calculates new velocity based on acceleration.  The order in this argument matchest the order in the paramenters for correct value assignment. Otherwise, when run, values for initialVelocity would be assigned to the acceleration, causing errors.
 
 // Pick up an error with how the function below is called and make it robust to such errors
-calculateNewVelocity = (initialVelocity, acceleration, time) => {
-  return initialVelocity + acceleration * time;
-};
 
 console.log(`Corrected New Velocity: ${updatedVelocity} km/h`);
 console.log(`Corrected New Distance: ${endingDistance} km`);
